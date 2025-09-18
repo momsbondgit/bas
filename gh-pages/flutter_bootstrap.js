f557de666d5e067a62fbcc2b9b4df4eea5d7f157
Your Correct GitHub Username <alieelpozo3@gmail.com>
2025-09-18 18:24:01 -0400

    Deploy fresh web build (no PWA) 2025-09-18T22:24:01Z

--- gh-pages/flutter_bootstrap.js
+++ gh-pages/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2170679349"
+    serviceWorkerVersion: "1170776882"
   }
 });
